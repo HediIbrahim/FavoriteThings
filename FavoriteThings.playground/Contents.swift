f6f20d4f9d593206abe0deff587bf02da495bb87
Hedi <HediIbrahim@users.noreply.github.com>
2017-04-14 00:38:51 -0400

    Add files via upload

---
+++ FavoriteThings.playground/Contents.swift
@@ -0,0 +1,25 @@
+var favoriteIceCream = "Strawberry"
+
+var favoriteDisneySong = "Let It Go"
+
+print("My favorite ice-cream flavor is vanilla")
+
+print("My favorite Disney song is Let It Go from Frozen")
+
+print("Hi uncle Kazbek, my favorite Disney song is Let It Go from Frozen, also my favorite ice-cream flavor is vanilla.")
+
+print("Hi mom! <3 you. My favorite Disney song is Let It Go from Frozen. My favorite ice-cream flavor is vanilla.")
+
+// string interpolation examples
+
+favoriteDisneySong = "A Whole New World"
+favoriteIceCream = "Chocolate"
+
+print("My favorite ice-cream flavor is \(favoriteIceCream)")
+
+print("My favorite Disney song is \(favoriteDisneySong) from Frozen")
+
+print("Hi uncle Kazbek, my favorite Disney song is \(favoriteDisneySong) from Frozen, also my favorite ice-cream flavor is \(favoriteIceCream).")
+
+print("Hi mom! <3 you. My favorite Disney song is \(favoriteDisneySong) from Frozen. My favorite ice-cream flavor is \(favoriteIceCream).")
+		
